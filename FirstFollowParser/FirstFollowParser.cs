5f6ad912a6d844965b1e6629d3a79ede0d7996dd
darkshot <ameh46870@gmail.com>
2020-01-23 05:48:55 +0330

    follow partialy done

--- FirstFollowParser/FirstFollowParser.cs
+++ FirstFollowParser/FirstFollowParser.cs
@@ -23,6 +23,13 @@ namespace FirstFollowParser
             }
 
         }
+        public void Follow()
+        {
+            foreach (var item in grammer.nonTerminals)
+            {
+                FollowOf(item);
+            }
+        }
 
         public void printFirst()
         {
@@ -31,7 +38,73 @@ namespace FirstFollowParser
                 first[i].print();
             }
         }
+        public void PrintFollow()
+        {
+            for (int i = 0; i < follow.Count; i++)
+            {
+                follow[i].print();
+            }
+        }
+        public HashSet<string> FollowOf(string source)
+        {
+            var oldFollow = follow.FirstOrDefault(e => e.variable == source);
+            if (oldFollow != null)
+            {
+                return oldFollow.setOfFollow;
+            }
+            var listOfFollows = new HashSet<string>();
 
+            for(int j=0;j<grammer.grammer.Count;j++)
+            {
+                var rule = grammer.grammer[j];
+                if (j == 0&&source==rule.source)
+                    listOfFollows.Add("$");
+                foreach (var product in rule.production.Where(e=>e.Contains(source)))
+                {
+                    var foundIndexes = new List<int>();
+                    for (int i = product.IndexOf(source); i > -1; i = product.IndexOf(source, i + 1))
+                    {
+                        foundIndexes.Add(i);
+                    }
+                    foreach (var index in foundIndexes)
+                    {
+                        if (index == product.Length - source.Length && source!=rule.source)
+                        {
+                            //the follow of rule owner
+                            listOfFollows.UnionWith(FollowOf(rule.source));
+                        }
+                        else if(index + source.Length<product.Length&&grammer.nonTerminals.Contains(product[index+source.Length].ToString()))//found nonterminals
+                        {
+                            var firstof = FirstOf(product[index + 1].ToString());
+                            if (product.Length>index+2&&product[index + 2].ToString() == "'")
+                            {
+                                firstof = FirstOf(product.Substring(index+1,2));
+                            }
+                            //
+                            listOfFollows.UnionWith(firstof);
+                            if (firstof.Contains("!"))
+                            {
+                                listOfFollows.UnionWith(FollowOf(rule.source));
+                            }
+                        }
+                        var term = grammer.terminals.FirstOrDefault(product.Substring(index).Contains);
+                        if (term != null)//found terminals
+                        {
+                            listOfFollows.Add(term);
+                            //
+                        }
+                    }
+                }
+            }
+
+            listOfFollows=replaceEpislon(listOfFollows);
+
+
+            Follow fi = new Follow(source);
+            fi.setOfFollow = listOfFollows;
+            follow.Add(fi);
+            return listOfFollows;
+        }
 
         public HashSet<string> FirstOf(string source)
         {
@@ -75,7 +148,20 @@ namespace FirstFollowParser
         }
 
 
+        public static HashSet<String> replaceEpislon(HashSet<String> x)
+        {
+            var t = x.ToArray();
 
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (t[i] == "!")
+                {
+                    t[i]="$";
+                }
+
+            }
+            return new HashSet<string>(t);
+        }
 
     }
 }
--- FirstFollowParser/Program.cs
+++ FirstFollowParser/Program.cs
@@ -6,7 +6,7 @@ namespace FirstFollowParser
     {
         static void Main(string[] args)
         {
-            GrammarParser grammer = new GrammarParser(@"C:\Users\darkshot\source\repos\LL1Parser\ParserTests\Sample3.in");
+            GrammarParser grammer = new GrammarParser(@"C:\Users\darkshot\source\repos\LL1Parser\ParserTests\Sample5.in");
             Console.WriteLine(string.Join(",", grammer.nonTerminals));
             Console.WriteLine(string.Join(",", grammer.terminals));
             Console.WriteLine(grammer.grammer.Count);
@@ -16,6 +16,11 @@ namespace FirstFollowParser
             var fifo = new FirstFollowParser(grammer);
             fifo.First();
             fifo.printFirst();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            fifo.Follow();
+            fifo.PrintFollow();
 
         }
     }
